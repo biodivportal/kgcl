9ade15553ca2a4f1df7e7e3a2efaea85dbc4aadd
Syphax <gs_bouazzouni@esi.dz>
2025-02-11 04:50:28 +0100

    add ClassDeletion implementation

--- src/kgcl_schema/datamodel/kgcl.py
+++ src/kgcl_schema/datamodel/kgcl.py
@@ -309,6 +309,9 @@ class ObjectPropertyCreationId(NodeCreationId):
 class NodeDeletionId(NodeChangeId):
     pass
 
+class ClassDeletionId(NodeDeletionId):
+    pass
+
 
 class NodeDirectMergeId(NodeObsoletionId):
     pass
@@ -2530,6 +2533,36 @@ class NodeDeletion(NodeChange):
 
         super().__post_init__(**kwargs)
         self.type = str(self.class_name)
+
+@dataclass
+class ClassDeletion(NodeDeletion):
+    """
+    A node deletion where the owl type is 'owl:Class'
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = KGCL.ClassDeletion
+    class_class_curie: ClassVar[str] = "kgcl:ClassDeletion"
+    class_name: ClassVar[str] = "ClassDeletion"
+    class_model_uri: ClassVar[URIRef] = KGCL.ClassDeletion
+
+    id: Union[str, ClassDeletionId] = None
+    superclass: Optional[Union[str, NodeId]] = None
+    change_description: Optional[str] = None
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if self._is_empty(self.id):
+            self.MissingRequiredField("id")
+        if not isinstance(self.id, ClassDeletionId):
+            self.id = ClassDeletionId(self.id)
+
+        if self.superclass is not None and not isinstance(self.superclass, NodeId):
+            self.superclass = NodeId(self.superclass)
+
+        if self.change_description is not None and not isinstance(self.change_description, str):
+            self.change_description = str(self.change_description)
+
+        super().__post_init__(**kwargs)
 
 
 @dataclass(repr=False)
